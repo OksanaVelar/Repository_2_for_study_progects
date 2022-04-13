ebc99b1a7fabae00a7d0e903d5ef4372dc5f391c
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-13 21:07:35 +0300

    Исправлены синтаксические ошибки кода

---
+++ homework/task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = {2,3,7};
+int size = 3;
+int index = 0;
+int max = numbers[0];
+Console.WriteLine("В массиве чисел 2,3,7 максимальное значение:");
+while(index < size){
+    if(numbers[index] > max){
+        max = numbers[index];
+    }
+    index = index + 1;
+}
+Console.Write($"{max}");
+  
