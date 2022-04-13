d1e8fe4823335e232bcd316ca0d311d2aff780c5
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-14 00:32:23 +0300

    Добавлен Program.cs

--- homework/task_6/Program.cs
+++ homework/task_6/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число для определения его четности:  ");
+string usernumber = Console.ReadLine();
+int firstArg = Convert.ToInt32(usernumber);
+
+int result = firstArg%2;
+
+if(result == 0)
+{
+    Console.WriteLine($"Число {firstArg} четное");
+}
+else
+{
+    Console.WriteLine($"Число {firstArg} нечетное");
+}
+
+
