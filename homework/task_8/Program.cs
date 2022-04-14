a476859c4085bd1c94f807cc123f169104a19575
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-14 04:00:32 +0300

    Добавлены неотслеживаемые файлы

--- homework/task_8/Program.cs
+++ homework/task_8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите максимальное число исходного числового множества:  ");
+string usernumber = Console.ReadLine();
+int firstArg = Convert.ToInt32(usernumber);
+int[] numbers = {1,firstArg};
+int max = firstArg;
+int index = 0;
+int size = firstArg;
+while (index < size){
+if(numbers[index]%2 == 0){
+    Console.WriteLine(numbers[index]); 
+    index = index + 1;
+}
+
+}
