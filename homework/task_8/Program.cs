c88b46e38a71e9c579351a703f91e646c7797361
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-16 20:06:23 +0300

    Создание Program.cs

--- homework/task_8/Program.cs
+++ homework/task_8/Program.cs
@@ -1,11 +1,17 @@
-﻿Console.WriteLine("Введите максимальное число исходного числового множества:  ");
+﻿// Вывод случайного числа и нахождение всех четных чисел в промежутке от этого числа до 1
+
+Console.WriteLine("Введите число:  ");
 string usernumber = Console.ReadLine();
 int firstArg = Convert.ToInt32(usernumber);
-int[] numbers = {1,firstArg};
-int index = 1;
-int size = firstArg;
-while (index < size){
-if(numbers[index]%2 == 0){
-    Console.WriteLine($"{numbers[index]}");}
-    index = index + 1;
+int result = firstArg%2;
+
+if(result == 0)
+{
+    Console.WriteLine($"{firstArg}");
+   List<int> numbers = new List<int>(); 
 }
+else
+{
+    Console.WriteLine($"{_}");
+}
+
