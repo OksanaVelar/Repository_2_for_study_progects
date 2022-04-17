b406208aec9b526613c670d65916b81d70ea2b06
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-17 11:13:31 +0300

    Написание кода

---
+++ homework/Task_4_16.04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ввод цифры и проверка ее соответствия субботе и воскресенью
+int n = 7;
+int g = 6;
+Console.WriteLine("Введите номер дня недели и нажмите клавишу Enter");
+int usernumber = int.Parse(Console.ReadLine());
+
+if((usernumber == g)|(usernumber == n))
+{
+Console.WriteLine("Введенный Вами номер соответствует выходному дню");
+}
+else
+{
+Console.WriteLine("Введенный Вами номер не соответствует выходному дню");
+}
