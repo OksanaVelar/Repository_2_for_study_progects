746678e0d3a5e8d40acd63a3f68717afdbaf70e5
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-19 17:08:51 +0300

    Добавлены неотслеживаемые файлы

--- homework/task_21_19.04/Program.cs
+++ homework/task_21_19.04/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Расстояние между точками в 3D пространстве
+int x1 = 5;
+int y1 = 17;
+int z1 = 8;
+int x2 = 15;
+int y2 = 24;
+int z2 = 73;
+double Length = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+Console.Write(Length);
