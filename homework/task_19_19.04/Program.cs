d849b927f28d1492ad19ba89cf794518f0b07165
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-19 15:09:44 +0300

    Написание кода

--- homework/task_19_19.04/Program.cs
+++ homework/task_19_19.04/Program.cs
@@ -1,2 +1,17 @@
-﻿// Проверка массива на полиндром
-Console.WriteLine("Hello, World!");
+﻿// Проверка числа - является ли полиндромом
+Console.WriteLine("Введите пятизначное число и нажмите клавишу Enter");
+int usernumber = int.Parse(Console.ReadLine());
+int a = usernumber%10;
+int b = usernumber/10;
+int c = usernumber/100;
+int d = usernumber/1000;
+int r = usernumber/10000;
+
+if((a==r)&(b==d))
+{
+    Console.WriteLine("Введенное Вами число является полиндромом");
+}
+else
+{
+    Console.WriteLine("Введенное Вами число не является полиндромом");
+}
