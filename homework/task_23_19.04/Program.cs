b69cdd6a393cebb8246f6e4e15446ba65c2316d9
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-19 18:03:56 +0300

    Написание кода

--- homework/task_23_19.04/Program.cs
+++ homework/task_23_19.04/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создание таблицы кубов значений от 1 до N
+Console.WriteLine("Введите число и нажмите клавишу Enter");
+int usernumber = int.Parse(Console.ReadLine());
+int r = 1;
+while(r <= usernumber)
+{
+  if(r < usernumber)
+   {
+    Console.Write(Math.Pow(r,3));
+    r++;
+   }
+  else
+   {
+    Console.Write(Math.Pow(usernumber,3));
+   }
+}
+
