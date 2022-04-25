ac2713a539b3742e5e01b10df81dfe26d6b65254
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-25 08:47:42 +0300

    Введен код

--- homework/task_25_23.04/Program.cs
+++ homework/task_25_23.04/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На входе два числа; На выходе - первое возводится в степень, равную второму числу
+
+Console.WriteLine("Введите первое число и нажмите клавишу Enter");
+  int A = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+Console.WriteLine("Введите второе число и нажмите клавишу Enter");
+  int B = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+int result = (int)Math.Pow(A,B);
+    Console.WriteLine(result);
+
