260081c4afd955a5bf098c40582ee836884d2245
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-25 14:59:58 +0300

    Создание Program.cs

--- homework/task_27_23.04/Program.cs
+++ homework/task_27_23.04/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На входе - число; на выходе - сумма цифр в этом числе
+
+//Console.WriteLine("Введите число и нажмите клавишу Enter");
+//int numberA = int.Parse(Console.ReadLine());
+int a = 3864;
+string str = a.ToString();
+int[] b = new int[str.Length];
+for(int i = 0; i  < str.Length; i++)
+{
+    b[i] = int.Parse(str[i].ToString());
+}
+//Console.WriteLine(str[a]);
+
+
+    
+
+
