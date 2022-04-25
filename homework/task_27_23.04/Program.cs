3a3adde412a433b13a318f4dbeea9e584518e590
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-25 18:00:00 +0300

    Создан код

--- homework/task_27_23.04/Program.cs
+++ homework/task_27_23.04/Program.cs
@@ -1,13 +1,15 @@
 ﻿// На входе - число; на выходе - сумма цифр в этом числе
 
-//Console.WriteLine("Введите число и нажмите клавишу Enter");
-//int numberA = int.Parse(Console.ReadLine());
-int a = 3864;
-string str = a.ToString();
+Console.WriteLine("Введите число и нажмите клавишу Enter");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+while(a > 0)
+{
+    sum = sum + a%10;
+    a /= 10;
+}
+Console.WriteLine($"Сумма чиcел в введенном Вами числе = {sum}");
 
-int[]array = new int[] {2,4,5,1,6,77,95,43};
-var str = string.Join(" ", array);
-Console.WriteLine(str);
 
     
 
