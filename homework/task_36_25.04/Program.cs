288f1828b49228b8ca30e8bd4e2b892ca1ce7dcb
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-27 02:53:51 +0300

    Создан код программы

--- homework/task_36_25.04/Program.cs
+++ homework/task_36_25.04/Program.cs
@@ -1,22 +1,20 @@
 ﻿//Задан массив со случайными числами. 
 //Требуется показать сумму элементов на нечетных позициях
 Console.Write("Дан массив случайных чисел: ");
-int[] mass = new int[10];
+int[] mass = new int[12];
 int sum = 0;
+
 for(int i = 0; i < mass.Length; i++)
 {
-   mass[i] = new Random().Next(200, 211);
+   mass[i] = new Random().Next(200, 250);
    Console.Write(mass[i]+ " ");
 }
 Console.WriteLine();
-for(int i = 0; i < mass.Length; i++)
+for(int i = 1; i < mass.Length; i = i + 2)
 {
-    if(mass[i]%2 == 0)
-    {
-        //sum += mass[i]; 
-        sum++;
-    }
+    sum++;
 }
 Console.WriteLine();
-Console.WriteLine($"Сумма четных чисел в массиве = {sum}");
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях в массиве = {sum}");
 Console.WriteLine();
+
