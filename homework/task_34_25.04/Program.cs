367de1f346c1f5cfd44c902017579d70511b27c2
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-27 01:09:16 +0300

    Создан код программы

--- homework/task_34_25.04/Program.cs
+++ homework/task_34_25.04/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задан массив со случайными трехзначными числами. 
+//Требуется показать кол-во четных чисел в нем.
+Console.Write("Дан массив случайных чисел: ");
+int[] mass = new int[10];
+int sum = 0;
+for(int i = 0; i < mass.Length; i++)
+{
+   mass[i] = new Random().Next(200, 211);
+   Console.Write(mass[i]+ " ");
+}
+Console.WriteLine();
+for(int i = 0; i < mass.Length; i++)
+{
+    if(mass[i]%2 == 0)
+    {
+        //sum += mass[i]; 
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма четных чисел в массиве = {sum}");
+Console.WriteLine();
+
+
+
