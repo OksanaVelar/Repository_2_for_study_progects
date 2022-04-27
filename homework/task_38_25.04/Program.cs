9b8f5195464f845c05280d0f3ceb2524744badd0
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-04-27 17:47:33 +0300

    Создан код программы

--- homework/task_38_25.04/Program.cs
+++ homework/task_38_25.04/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В массиве вещественных чисел найти разность между max и min значениями
+Console.WriteLine("Дан массив случайных чисел: ");
+double[] mass = new double[5];
+//double A = mass[0];
+//double B = mass[mass.Length - 1];
+//double Z = B - A;
+for(int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().NextDouble();
+    Console.WriteLine(mass[i]);
+}
+
+Console.WriteLine();
+
+double temp;
+for(int i = 0; i < mass.Length - 1; i++)
+{
+for(int j = i+1; j < mass.Length; j++)
+
+if(mass[i] > mass[j])
+{
+    temp = mass[i];
+    mass[i] = mass[j];
+    mass[j] = temp;
+}
+}
+Console.WriteLine("Отсортированный массив:");
+
+for(int i = 0; i < mass.Length; i++)
+{
+   Console.WriteLine(mass[i]);
+}
+Console.WriteLine();
+double A = mass[0];
+double B = mass[mass.Length - 1];
+double Z = B - A;
+Console.WriteLine($"Разность max и min значений в массиве = {Z}");
+
