feb23596c28e2958377ba02613f4b8c6a4767478
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-05 21:58:49 +0300

    Создан код программы

--- homework/task_43_30.04/Program.cs
+++ homework/task_43_30.04/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти точку пересечения двух прямых, заданных как: y = k1*x + b1 и y = k2*x + b2
+Console.WriteLine("Введите значение переменной k1: ");
+double k1 = double.Parse(Console.ReadLine());
+//Console.WriteLine($"k1 = {k1}");
+Console.WriteLine("Введите значение переменной k2: ");
+double k2 = double.Parse(Console.ReadLine());
+//Console.WriteLine($"k2 = {k2}");
+Console.WriteLine("Введите значение переменной b1: ");
+double b1 = double.Parse(Console.ReadLine());
+//Console.WriteLine($"b1 = {b1}");
+Console.WriteLine("Введите значение переменной b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine($"k1 = {k1}, k2 = {k2}, b1 = {b1}, b2 = {b2}");
+double X = 0;
+double Y1;
+double Y2;
+Y1 = k1*X+b1;
+Y2 = k2*X+b2;
+X = (b2 - b1)/(k1-k2);
+double Y = k1*X + b1;
+if(k1==k2)
+{
+Console.WriteLine("Функции не пересекаются");
+}
+    Console.WriteLine(X+", "+Y);
