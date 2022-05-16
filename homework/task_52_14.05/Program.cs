218adeef525d47946d0a3c7a94c54c14b01a08ce
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-17 02:55:01 +0300

    Создание кода программы

--- homework/task_52_14.05/Program.cs
+++ homework/task_52_14.05/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В каждом столбце двумерного массива целых чисел найти значение 
+// среднего арифметического
+int a = 3; 
+int b = 3;
+double sum = 0;
+double result = 0;
+
+int [,] mass = new int[a,b];
+void Print (int[,] mass)
+{
+   for (int i = 0; i < mass.GetLength(0); i++)
+   {
+       for (int j = 0; j < mass.GetLength(1); j++)
+       {
+           mass[i,j] = new Random().Next(1,100);
+           Console.Write(mass[i,j] + " ");
+       }
+       Console.WriteLine();
+   }
+   
+   for (int j = 0; j < mass.GetLength(1); j++)
+   {
+       for (int i = 0; i < mass.GetLength(0); i++)
+       {
+           sum = sum + mass[i,j];
+       }
+   result = sum/mass.GetLength(0);
+   Console.WriteLine();
+   Console.Write(Math.Round(result, 2) + " ");
+   }
+}
+Print(mass);
+
+
