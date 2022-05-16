4304e1d366bdf46c714fee6a52c1184c45ccf916
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-16 20:45:26 +0300

    Создан код программы

--- homework/task_47_14.05/Program.cs
+++ homework/task_47_14.05/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать двум.масс. размером m*n, заполненный случайными вещественными числами
+int n = 5; 
+int m = 5;
+
+double [,] mass = new double[m,n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i,j] = Math.Round (new Random().NextDouble()*100,3);
+        Console.Write(mass[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
