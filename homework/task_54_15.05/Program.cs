5fcb6179e25978ec458415c8e9edcd8aec2a5a93
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-21 23:25:02 +0300

    Создание кода

--- homework/task_54_15.05/Program.cs
+++ homework/task_54_15.05/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В двумерном массиве упорядочить по убыванию элементы каждой строки
+int a = 2; 
+int b = 3;
+
+int[,] mass = new int[a,b];
+
+   int count1 = mass.GetLength(1);
+   int count0 = mass.GetLength(0);
+    for (int i = 0; i < count0; i++)
+    {
+        for (int j = 0; j < count1; j++)
+        {
+            mass[i,j] = new Random().Next(1,10);
+        }
+    }
+
+int [,] PrintArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("------------------");
+    return matr;
+}
+
+Console.WriteLine();
+int[,] SelectionSort(int[,] mass)
+{
+  int count1 = mass.GetLength(1);
+  int count0 = mass.GetLength(0);
+
+    for (int i = 0; i < count0; i++)
+    {
+        for (int j = 0; j < count1-1; j++)
+        {
+            for (int k = j+1; k < count1; k++)
+            {
+                if(mass[i,k] > mass[i,j])
+                {
+                    int temporary = mass[i,j];
+                    mass[i,j] = mass[i,k];
+                    mass[i,k] = temporary;
+                }
+            }
+        }
+    }
+    return mass;
+}
+
+PrintArray (mass);
+PrintArray (SelectionSort(mass));
+
+
