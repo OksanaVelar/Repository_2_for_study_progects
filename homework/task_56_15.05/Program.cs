eb7baa6081186e407e6c5b372288fa948f5006e2
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-22 05:52:50 +0300

    Создание Program.cs

--- homework/task_56_15.05/Program.cs
+++ homework/task_56_15.05/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В массиве найти строчку с наименьшей суммой значений элементов
+
+int a = 3; 
+int b = 3;
+
+int[,] mass = new int[a,b];
+int count1 = mass.GetLength(1);
+int count0 = mass.GetLength(0);
+   for (int i = 0; i < count0; i++)
+   {
+       for (int j = 0;j < count1; j++)
+       {
+           mass[i,j] = new Random().Next(1,10);
+       }
+       Console.WriteLine();
+   }
+
+int[,] PrintMass(int[,] mass)
+{
+    for (int i = 0; i < count0; i++)
+    {
+        for (int j = 0; j < count1; j++)
+        {
+            Console.Write(mass[i,j] + " ");
+        }
+        Console.WriteLine();
+        return mass;
+    }
+}
+
