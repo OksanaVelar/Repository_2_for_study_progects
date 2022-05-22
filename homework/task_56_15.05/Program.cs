014e20c1921392a80a573f52548ee93619d98390
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-22 08:04:47 +0300

    Создание Program.cs

--- homework/task_56_15.05/Program.cs
+++ homework/task_56_15.05/Program.cs
@@ -1,30 +1,52 @@
 ﻿// В массиве найти строчку с наименьшей суммой значений элементов
 
-int a = 3; 
+int a = 3;
 int b = 3;
 
-int[,] mass = new int[a,b];
+int[,] mass = new int[a, b];
 int count1 = mass.GetLength(1);
 int count0 = mass.GetLength(0);
-   for (int i = 0; i < count0; i++)
-   {
-       for (int j = 0;j < count1; j++)
-       {
-           mass[i,j] = new Random().Next(1,10);
-       }
-       Console.WriteLine();
-   }
-
+for (int i = 0; i < count0; i++)
+{
+    for (int j = 0; j < count1; j++)
+    {
+        mass[i, j] = new Random().Next(1, 10);
+    }
+}
 int[,] PrintMass(int[,] mass)
 {
     for (int i = 0; i < count0; i++)
     {
         for (int j = 0; j < count1; j++)
         {
-            Console.Write(mass[i,j] + " ");
+            Console.Write(mass[i, j] + " ");
         }
         Console.WriteLine();
-        return mass;
     }
+
+    return mass;
 }
+PrintMass(mass);
+Sum(mass);
+void Sum(int[,] array)
+{
+    int temp = 1000;
+    int minstring = 0;
+    
+    for (int i = 0; i < count0; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < count1; j++)
+        {
+            sum = sum + array[i, j];
+        }
+        if (sum < temp)
+        {
+            temp = sum;
+            minstring = i;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой: {minstring + 1}");
+}
+
 
--- homework/task_60_15.05/Program.cs
+++ homework/task_60_15.05/Program.cs
@@ -1,3 +1,43 @@
 ﻿// Задать трехмерный массив из неповторяющихся двузначных чисел. 
 // Вывести строки массива с добавлением индекса каждого элемента.
+int a = 2; 
+int b = 3;
+int c = 3;
+
+int[,,] mass = new int[a,b,c];
+
+   int count1 = mass.GetLength(1);
+   int count0 = mass.GetLength(0);
+   int count2 = mass.GetLength(2);
+   int r = 20;
+    for (int i = 0; i < count0; i++)
+    {
+        for (int j = 0; j < count1; j++)
+        {
+            for (int k = 0; k < count2; k++)
+            {
+                mass[i,j,k] = r;
+                r = r+2;
+            }
+        }
+    }
+
+int[,,] PrintMass(int[,,] mass)
+{
+    for (int i = 0; i < count0; i++)
+    {
+        for (int j = 0; j < count1; j++)
+        {
+            for (int k = 0; k < count2; k++)
+            {
+               Console.Write($"{mass[i,j,k]}({i}, {j}, {k}) "); 
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    return mass;
+}
+PrintMass (mass);
+Console.WriteLine();
 
