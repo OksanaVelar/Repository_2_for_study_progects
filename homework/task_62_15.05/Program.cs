8428604c8604acf724492a7d6a54b5d41d00d066
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-24 22:04:55 +0300

    Создание Program.cs

--- homework/task_62_15.05/Program.cs
+++ homework/task_62_15.05/Program.cs
@@ -1,52 +1,28 @@
 ﻿// Заполнить спирально массив 4 / 4 
-// int b = 4;
-// int c = 4;
 
-// int[,] mass = new int[b,c];
-// for (int i = 0; i < length; i++)
-// {
-    
-// }
+int a = 4;
+int b = 4;
+int[,] Spiral = new int[a, b];
 
-int[,] Snail = new int[7, 7];
-int s = 1;
-int j = 0;
-int i = 0;
-// for (int i = j; i < 4; i++)
-// {
-    for (j = 0; j < 4; j++)
-    {
-        Snail[0,j] = s;
-        s++;
-    }
-    for (i = 1; i < 4; i++)
-    {
-        Snail[i, 3] = s;
-        s++;
-    }
-    for (j = 2; j >= 0; j--)
-    {
-        Snail[3, j] = s;
-        s++;
-    }
-    for (i = 2; i > 0; i--)
-    {
-        Snail[i, 0] = s;
-        s++;
-    }
-    for (j = 1; j < 3; j++)
-    {
-        Snail[1,j] = s;
-        s++;
-    }
-    for(j = 2; j > 0; j--)
-    {
-        Snail[2, j] = s;
-        s++;
-    }
-//     break;
+int step = 1;
+int start = Spiral.GetLength(0)/2; 
+int newpoint; 
 
-// }
+PrintArray(Spiral);
+
+for (int i = start; i <= Spiral.GetLength(0);i++)
+{
+    for (int j = start; j <= Spiral.GetLength(1);j++)
+    {   
+        for (int k = start; k <= Spiral.GetLength(0);k--)
+        {
+           for (int z = start; z <= Spiral.GetLength(0);z--)
+           {
+               newpoint = start+step;
+           }
+        }
+    }
+}
 int [,] PrintArray(int [,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
@@ -57,7 +33,8 @@ int [,] PrintArray(int [,] matr)
         }
         Console.WriteLine();
     }
-    Console.WriteLine("------------------");
+    //Console.WriteLine("------------------");
     return matr;
 }
-PrintArray(Snail);
+
+
