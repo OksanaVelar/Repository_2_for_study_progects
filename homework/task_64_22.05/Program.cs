fce851c1b4212cff4c174beea79e6372b95afbc7
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-24 23:42:34 +0300

    Создание кода программы

--- homework/task_64_22.05/Program.cs
+++ homework/task_64_22.05/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать значения M,N и вывести все натуральные числа в промежутке между ними
+Console.WriteLine("Обозначте начало убывающего массива чисел (n) - введите число его верхней границы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Обозначте конец массива (m) - введите число его нижней границы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int n, int m)
+{
+    if(m-1 == n)
+    {
+    return;
+    }
+    Console.Write(n + " ");
+    PrintNumbers(n-1, m);
+}
+Console.WriteLine("________________");
+Console.WriteLine();
+Console.WriteLine("Массив натуральных чисел в заданном промежутке: ");
+PrintNumbers(n,m);
+
+
+
+
+
