29f777ece503d136dc2e9962f3f26a5e857e6bd9
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-05-26 10:04:05 +0300

    Скорректиррован код программы

--- homework/task_66_22.05/Program.cs
+++ homework/task_66_22.05/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задать значения M N , найти сумму натуральных значений в заданном промежутке
+Console.WriteLine("Обозначте начало убывающего массива чисел (n) - введите число его верхней границы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Обозначте конец массива (m) - введите число его нижней границы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+void PrintNumbers(int n, int m)
+{
+    if (m - 1 == n)
+    {
+        return;
+    }
+        Console.Write(n + " ");
+    
+    PrintNumbers(n - 1, m);
+    if(n>0)
+    {
+    sum = sum+n;
+    }
+    //Console.WriteLine();
+    Console.Write(" - " + sum);
+}
+
+Console.WriteLine();
+Console.WriteLine("Массив натуральных чисел в заданном промежутке: ");
+
+
+PrintNumbers(n, m);
+
+
+
+
+
